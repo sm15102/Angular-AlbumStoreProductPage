1f42936780a188ce4044477c21259f3e2a5ff0b3
Salem Maglic <sm15102@etf.unsa.ba>
2018-10-22 01:16:07 +0200

    Module Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component : ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -22,6 +29,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes)
 
   ],
   providers: [ ProductService ],

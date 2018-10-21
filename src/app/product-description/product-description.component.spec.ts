7a24a9ac664b9ba543dd2fae60b2ad69630c8919
Salem Maglic <sm15102@etf.unsa.ba>
2018-10-21 23:27:12 +0200

    Module COmpleted

--- src/app/product-description/product-description.component.spec.ts
+++ src/app/product-description/product-description.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductDescriptionComponent } from './product-description.component';
 
 describe('ProductDescriptionComponent', () => {
